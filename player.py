f7d426f8e01c98b9a45a3718451d0d1fc81984ac
alina234-ev <alina.chudnetsova@ev-frohnau.de>
2026-03-02 09:25:25 +0100

    kkddk

--- player.py
+++ player.py
@@ -1,19 +1,1 @@
-# Tkinter: Python-Bibliothek für grafische Oberflächen
-from tkinter import *
-from tkinter import colorchooser
-import pygame
-
-pygame.mixer.init()
-pygame.mixer.music.load("hintergrundmusik.mp3")
-pygame.mixer.music.play(-1)
-
-# Hauptfenster erstellen
-root = Tk()
-root.title("Labyrinth")
-root.geometry("600x600")
-
-farbe = colorchooser.askcolor(title="Wähle eine Hintergrundfarbe")[1] 
-if farbe:  
-    root.configure(bg=farbe)
-
 
